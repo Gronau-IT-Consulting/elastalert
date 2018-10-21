7da67a1433ba426d9bdf40885a32934c2a270b49
siemonster <siemonster@users.noreply.github.com>
2018-10-22 09:35:11 +1100

    Update elasticsearch_client.js

--- src/common/elasticsearch_client.js
+++ src/common/elasticsearch_client.js
@@ -3,7 +3,7 @@ import config from './config';
 
 export function getClient() {
   var client = new elasticsearch.Client({
-    hosts: [ `http://${config.get('es_host')}:${config.get('es_port')}`]
+    hosts: [ `${config.get('protocol')}://${config.get('auth')}@${config.get('es_host')}:${config.get('es_port')}`]
   });
   return client;
 }
